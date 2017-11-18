6ca4d7ee9def83c9fabd45942f00d68bb7527784
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-11-18 23:18:05 +0000

    Initialising a chess board

---
+++ RhinoChessEngine/RhinoChessEngine/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RhinoChessEngine
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AppPanel_LayoutUpdated(object sender, object e)
+        {
+            ChessBoard.Width = Math.Min(AppPanel.ActualWidth, AppPanel.ActualHeight);
+            ChessBoard.Height = Math.Min(AppPanel.ActualWidth, AppPanel.ActualHeight);
+        }
+    }
+}
